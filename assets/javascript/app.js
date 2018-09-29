a0fb7a4c3612708d65b3ac1e6fe6e6545104e274
Smushed <smushedcode@gmail.com>
2018-09-29 17:41:02 -0500

    Initializing the files

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//Have the questions as an array that you access with a forloop for every page that is scrolled through
+var questionArray = [{
+    question: "test question 1",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"]
+}]
